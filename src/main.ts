4962fea90bb81a7dfb38c95f3fcb654a6075acf3
only4sim <vquanli@126.com>
2023-12-27 23:42:03 +0100

    Make the logistic regression without zigmoid.

--- src/main.ts
+++ src/main.ts
@@ -23,20 +23,20 @@ async function main() {
 
     console.log('o1js loaded');
 
-    console.log('compiling...');
+    // console.log('compiling...');
 
-    const { verificationKey } = await ArgMax.compile();
+    // const { verificationKey } = await ArgMax.compile();
 
-    console.log('making proof 0')
+    // console.log('making proof 0')
 
-    let arr = [Field(1), Field(2), Field(3), Field(4), Field(5)];
-    const proof = await ArgMax.argMax(arr);
+    // let arr = [Field(1), Field(2), Field(3), Field(4), Field(5)];
+    // const proof = await ArgMax.argMax(arr);
 
-    console.log('verifying proof 0');
+    // console.log('verifying proof 0');
 
-    console.log('proof 0 data', proof.publicOutput.toString());
-    const ok = await verify(proof.toJSON(), verificationKey);
-    console.log('ok', ok);
+    // console.log('proof 0 data', proof.publicOutput.toString());
+    // const ok = await verify(proof.toJSON(), verificationKey);
+    // console.log('ok', ok);
 
     // console.log('compiling...');
     // const { verificationKey: verificationKey2 } = await Relu.compile();
@@ -121,6 +121,36 @@ async function main() {
 
     // const ok6 = await verify(proof6.toJSON(), verificationKey6);
     // console.log('ok', ok6);
+
+    // let input = [Field(1), Field(2), Field(3), Field(4), Field(5)];
+    // let coefficients = [Field(1), Field(2), Field(3), Field(4), Field(5)];
+    // const { verificationKey: verificationKey7 } = await Dense.compile();
+
+    // console.log('making proof 7')
+    // const proof7 = await Dense.dense(input, coefficients);
+
+    // console.log('verifying proof 7');
+
+    // let output7 = Field(175964519);
+    // proof7.publicOutput.assertEquals(output7);
+
+    // const ok7 = await verify(proof7.toJSON(), verificationKey7);
+    // console.log('ok', ok7);
+
+    let input = [Field(2), Field(3)];
+    const { verificationKey: verificationKey8 } = await LogisticRegression.compile();
+
+    console.log('making proof 8')
+    const proof8 = await LogisticRegression.predict(input);
+
+    console.log('verifying proof 8');
+
+    let output8 = Field(0);
+    proof8.publicOutput.assertEquals(output8);
+
+    const ok8 = await verify(proof8.toJSON(), verificationKey8);
+    console.log('ok', ok8);
+
 
 
 
@@ -220,13 +250,28 @@ methods: {
 
 const zigmoid = (input: Field, n: Field): Field => {
   // tmp = 198695283 * n**2 - 1570683 * n * input - 4001354 * input * input;
-  let tmp = n.square().mul(198695283).sub(input.mul(n).mul(1570683)).sub(input.square().mul(4001354));
+  let tmp = n.square().mul(199).sub(input.mul(n).mul(2)).sub(input.square().mul(4));
   // out * (n**2) * (10**9) + remainder === 502073021 * n**3 + in * tmp;
-  let dividend = Field(502073021).mul(n).mul(n).mul(n).add(input.mul(tmp));
-  let divisor = n.square().mul(1000000000);
+  let dividend = Field(502).mul(n).mul(n).mul(n).add(input.mul(tmp));
+  let divisor = n.square().mul(1000);
   let remainder = dividend;
   let quotient = new Field(0);
-  while (remainder.greaterThan(divisor)) {
+  Provable.log(input);
+  Provable.log(n);
+  Provable.log(tmp);
+  Provable.log(dividend);
+  Provable.log(divisor);
+  Provable.log(remainder);
+  Provable.log(quotient);
+
+  // while (remainder.greaterThan(divisor)) {
+  //     remainder = remainder.sub(divisor);
+  //     quotient = quotient.add(Field(1));
+  // }
+
+  let round = 100;
+
+  for (let i = 0; i<round && remainder.greaterThan(divisor); i++) {
       remainder = remainder.sub(divisor);
       quotient = quotient.add(Field(1));
   }
@@ -310,11 +355,13 @@ const LogisticRegression = ZkProgram({
     publicOutput: Field,
   methods: {
     predict: {
-      privateInputs: [Provable.Array(Field, 5)],
+      //The quantized input is scaled by sqrt of the n of the zigmoid, because the coefficients*input is scaled by n
+      privateInputs: [Provable.Array(Field, 2)],
 
       method(input: Field[]): Field {
-        const coefficients = [Field(175964519), Field(203090830)];
-        const intercept = new Field(-69312133);
+        const coefficients = [Field(175), Field(203)];
+        // Negative of the prediction may cause issues
+        const intercept = new Field(693);
         let dotProduct = new Field(0);
 
         for (let i = 0; i < coefficients.length; i++) {
@@ -322,7 +369,9 @@ const LogisticRegression = ZkProgram({
         }
 
         const z = dotProduct.add(intercept);
-        return zigmoid(z);
+        // Provable.log(z);
+        // const sigmoid = zigmoid(z, Field(1000000000));
+        return z;
     },
   },
 },
