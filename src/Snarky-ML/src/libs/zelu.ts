7deb58931d5c125fa380fa5e531cd1204f19a7f6
only4sim <vquanli@126.com>
2023-12-30 23:45:09 +0100

    New zelu

--- src/Snarky-ML/src/libs/zelu.ts
+++ src/Snarky-ML/src/libs/zelu.ts
@@ -17,19 +17,27 @@ import {
     Provable,
   } from 'o1js';
 
-  const Zelu = ZkProgram({
-    name: 'Zelu',
-    publicOutput: Field,
-  methods: {
-    zelu: {
-      privateInputs: [Field, Field],
+  // const Zelu = ZkProgram({
+  //   name: 'Zelu',
+  //   publicOutput: Field,
+  // methods: {
+  //   zelu: {
+  //     privateInputs: [Field, Field],
   
-      method(input, remainder): Field {
-        const n = Field(1000000000); // Assuming n is 10^9
-        remainder.assertLessThan(n);
-        let tmp = input.square().add(input.mul(n));
-        return tmp.sub(remainder).div(n);
-    },
-  },
-  },
-  });
+  //     method(input, remainder): Field {
+  //       const n = Field(1000000000); // Assuming n is 10^9
+  //       remainder.assertLessThan(n);
+  //       let tmp = input.square().add(input.mul(n));
+  //       return tmp.sub(remainder).div(n);
+  //   },
+  // },
+  // },
+  // });
+
+
+export const zelu =(input: Field, remainder: Field): Field =>{
+    const n = Field(1000000000); // Assuming n is 10^9
+    remainder.assertLessThan(n);
+    let tmp = input.square().add(input.mul(n));
+    return tmp.sub(remainder).div(n);
+};
